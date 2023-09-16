718b617a0eb1139d396750b0cb28e76391a538f2
gg-codermachine-gg <119689044+gg-codermachine-gg@users.noreply.github.com>
2023-09-16 17:50:06 +0300

    Update app to Next.js

---
+++ fakecord/tailwind.config.ts
@@ -0,0 +1,29 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    colors: {
+      "dark-gray": "#202225",
+      "middle-gray": "#2F3136",
+      "light-gray" :"#36393F",
+      "green" : "#3BA55D",
+      "userProfileGrey" : "#292B2F"
+    },
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+  },
+  
+  plugins: [],
+}
+export default config
+ 
