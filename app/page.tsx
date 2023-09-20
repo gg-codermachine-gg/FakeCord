16a976ee91f667103227cd96e78aeeea3fe5c123
gg-codermachine-gg <119689044+gg-codermachine-gg@users.noreply.github.com>
2023-09-20 21:34:02 +0300

    nothing

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
 
 function ServersList() {
   return (
-    <div className="flex flex-col min-h-screen w-[4.25rem] bg-green"></div>
+    <div className="flex flex-col min-h-screen w-[4.25rem] bg-dark-gray"></div>
   );
 }
 
@@ -26,33 +26,10 @@ function Screen() {
 
 function DirectMessges() {
   return (
-    <div className="bg-middle-gray w-[15rem] min-h-[57.8rem] flex flex-col justify-center">
-      <div className="flex flex-col w-full h-[5.5rem] items-center ">
-        <button className="w-[97%]  rounded-[0.31rem] h-[2.625rem] flex flex-row gap-5 text-[1.3rem] font-medium leading-6 tracking-tighter items-center pl-5 text-textColor hover:bg-hoverColor hover:text-white transition ease-out ">
-          <span>
-            <Image
-              src="/assets/friendsIcon.svg"
-              alt="My Icon"
-              width={22}
-              height={22}
-              
-            />
-          </span>
-          Friends
-        </button>
-        <button className="w-[97%]  rounded-sm h-[2.625rem] flex flex-row gap-5 text-[1.3rem] font-medium leading-6 tracking-tighter items-center pl-5 text-textColor hover:bg-hoverColor hover:text-white transition ease-out ">
-          <span>
-          <Image
-              src="/assets/nitroIcon.svg"
-              alt="My Icon"
-              width={22}
-              height={22}
-              
-            />
-          </span>
-          Nitro
-        </button>
-      </div>
+    <div className="bg-middle-gray w-[15rem] min-h-[57.8rem] flex flex-col items-center">
+      <OptionsButtons />
+      <FriendsList />
+      <div className="h-[5rem] bg-red-500"></div>
     </div>
   );
 }
@@ -65,6 +42,55 @@ function Search() {
         className="w-[13.75rem] h-7 p-3 bg-dark-gray font-medium text-sm tracking-tighter rounded-sm"
         placeholder="Find or start a conversation"
       />
+    </div>
+  );
+}
+
+function OptionsButtons({}) {
+  return (
+    <div className="flex flex-col w-full h-[5.5rem] items-center p-2">
+      <button className="w-[97%]  rounded-[0.31rem] h-[2.625rem] flex flex-row gap-5 text-[1.3rem] font-medium leading-6 tracking-tighter items-center pl-5 text-textColor hover:bg-hoverColor hover:text-white transition ease-out ">
+        <span>
+          <Image
+            src="/assets/friendsIcon.svg"
+            alt="My Icon"
+            width={22}
+            height={22}
+          />
+        </span>
+        Friends
+      </button>
+      <button className="w-[97%]  rounded-sm h-[2.625rem] flex flex-row gap-5 text-[1.3rem] font-medium leading-6 tracking-tighter items-center pl-5 text-textColor hover:bg-hoverColor hover:text-white transition ease-out ">
+        <span>
+          <Image
+            src="/assets/nitroIcon.svg"
+            alt="My Icon"
+            width={22}
+            height={22}
+          />
+        </span>
+        Nitro
+      </button>
+    </div>
+  );
+}
+
+function FriendsList({}) {
+  return (
+    <div className="h-[47.3rem] flex flex-row items-center">
+      <div className="flex flex-row gap-[4rem] h-2 w-full pl-3">
+        <h1 className="text-gray-600  text-md font-bold tracking-tight ">
+          DIRECT MESSAGES
+        </h1>
+        <button className="mt-1 mr-3">
+          <Image
+            src="/assets/plusIcon.svg"
+            alt="My Icon"
+            width={15}
+            height={15}
+          />
+        </button>
+      </div>
     </div>
   );
 }
